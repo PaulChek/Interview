4ee66d1e54884fd2e138acde76ccf89e40d31a61
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-07-26 01:10:29 +0700

    Add project files.

---
+++ SumOfTwoNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+given array [1,3,7,9,2]
+find summ of 11 => (3,4)
+*/
+namespace SumOfTwoNumbers {
+    class Program {
+        static void Main(string[] args) {
+            var summ = 11;
+            var arr = new int[] { 1, 3, 7, 9, 2 };
+
+            Console.WriteLine(Solution(arr, summ));
+        }
+        static (int, int) Solution(int[] arr, int summ) {
+            //using hashmapping
+            var hash = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++) {
+                var looking_for = summ - arr[i];
+                if (hash.ContainsKey(looking_for)) return (hash[looking_for], i);
+                hash[arr[i]] = i;
+            }
+
+            return (-1, -1);
+        }
+        static  (int, int) BadSolution(int[] arr, int summ) {
+            var (p1, p2) = (0,1);
+
+            for (; p1<arr.Length;) {
+                if (p2 >= arr.Length) { ++p1; p2 = p1 + 1; }
+                var target = summ - arr[p1];
+                if (target == arr[p2]) return (p1, p2);
+                p2++;
+            }
+
+            return (-1, -1);
+        }
+    }
+}
---
+++ WaterAndCharts/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+[1,8,6,2,9,4]
+find  biggest container!
+^
+|               
+|              |
+|     |        |
+|     |        |
+|     |  |     |
+|     |  |     |    
+|     |  |     |  |
+|     |  |     |  |
+|     |  |  |  |  |
+|__|__|__|__|__|__|___>
+   1  8  6  2  9  4
+*/
+namespace WaterAndCharts {
+    class Program {
+        static void Main(string[] args) {
+            var bars = new int[] { 6, 9, 3, 4, 5, 8 };
+            Console.WriteLine(string.Join("\n", OptimizeSolution(bars)));
+
+        }
+
+        static int OptimizeSolution(int[] arr) {
+            //shifting pointers area = min(a,b) * (bj - ai);
+            var max = 0;
+
+            for (int i = 0, j = arr.Length - 1; j > i;)
+                if (arr[i] < arr[j]) { max = Math.Max(max, arr[i] * (j - i)); i++; }
+                else { max = Math.Max(max, arr[j] * (j - i)); j--; };
+
+
+
+            return max;
+        }
+
+        static int BruteSolution(int[] arr) {
+            var v = 0;
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = i + 1; j < arr.Length; j++)
+                    v = Math.Max(v, Math.Min(arr[i], arr[j]) * (j - i));
+
+            return v;
+        }
+    }
+}
