316e1d502f1f741d76bdce773f875a4959700c34
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-07-29 00:48:08 +0700

    Palindrome

---
+++ AlmostPalindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AlmostPalindrome {
+    class Program {
+        static void Main(string[] args) {
+            var arrs = new (string, bool)[]{
+            ( "aaddaa", true ),
+            ( "aadgdaa", true ),
+            ("a", true),
+            ("abb", true),
+            ("abbbc", false),
+            ("aaaaba", true),
+            ("abaaba", true),
+            ("eedede", true), //*
+            ("abcba", true),
+            ("", true),
+            ("a man, a plan, a canal, panama", true),
+            ("cdbeeeabddddbaeedebdc",true)
+            };
+
+            foreach (var s in arrs)
+                Console.WriteLine(s.Item2 == IsAlmostPalindrome(s.Item1) ? 'V' : 'X');
+
+
+
+
+
+        }
+        private static bool IsAlmostPalindrome(string s, int secondChance = 0, int pL = 0, int pR = 0) {
+
+            if (secondChance > 1) return false;
+
+            pR = pR == 0 ? s.Length - 1 : pR;
+
+            while (pL <= pR) {
+                if (s[pL] != s[pR])
+                    return IsAlmostPalindrome(s, secondChance + 1, pL + 1, pR) || IsAlmostPalindrome(s, secondChance + 1, pL, pR - 1);
+                pL++;
+                pR--;
+            }
+
+            return true;
+
+        }
+    }
+}
---
+++ CheckIFpalindrome/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CheckIFpalindrome {
+    class Program {
+        static void Main(string[] args) {
+            var arrs = new (string, bool)[]{
+            ( "aaddaa", true ),
+            ( "aadGdaa", true ),
+            ("a", true),
+            ("abc", false),
+            ("aaaaba", false),
+            ("abaaba", true),
+            ("abcba", true),
+            ("", true),
+            ("eedede", true),
+            ("A man, a plan, a canal, Panama", true)
+            };
+            foreach (var s in arrs)
+                Console.WriteLine(s.Item2 == IsPalindrome2(s.Item1));
+
+        }
+
+        private static bool IsPalindrome1(string s) {
+            s = ClearString(s);
+            int pL = 0, pR = s.Length - 1;
+
+            while (pL <= pR)
+                if (s[pL++] != s[pR--]) return false;
+
+            return true;
+        }        
+        private static bool IsPalindrome2(string s) {
+            s = ClearString(s);
+            if (s.Length <= 1) return true;
+            
+            int start = s.Length / 2;
+            int pL = start - 1, pR = start + ((s.Length&1)==1?1:0);
+
+            while (pL>=0 && pR < s.Length) 
+                if (s[pL--] != s[pR++]) return false;
+            
+            return true;
+
+        }
+
+        private static string ClearString(string s) {
+            return Regex.Replace(s, @"[^\w]", "").ToLower();
+        }
+    }
+}
