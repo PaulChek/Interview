c141152477ed2abad4dd8c70f636da03f2938976
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-08-01 04:13:24 +0700

    removeInvalidparentesis

--- Stacks/ValidParentesis/Program.cs
+++ Stacks/ValidParentesis/Program.cs
@@ -6,9 +6,9 @@ using System.Text.RegularExpressions;
 namespace ValidParentesis {
     class Program {
         static void Main(string[] args) {
-            string ps = "({{{}}}[])";
+            string ps ="))((";
 
-            bool res = SolutionReg(ps);
+            var res = HowManyRemove(ps);
 
             Console.WriteLine(res);
         }
@@ -27,7 +27,26 @@ namespace ValidParentesis {
             return b.Count == 0;
         }
 
-        private static bool SolutionReg(string s, string z = "") => (z = Regex.Replace(s, @"\[\]|\{\}|\(\)", "")) == s ? z.Length == 0 : Solution(z);
+        private static bool SolutionReg(string s, string z = "") =>
+                         (z = Regex.Replace(s, @"\[\]|\{\}|\(\)", "")) == s ? z.Length == 0 : SolutionReg(z);
+
+        private static string HowManyRemove(string s) {
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < s.Length; i++) {
+                var c = s[i];
+                if (c == '(') stack.Push(i);
+                else if (c == ')')
+                    if (stack.Count > 0) stack.Pop();
+                    else { s = s.Remove(i, 1);  i--; }
+            }
+
+            foreach (var item in stack)
+                s = s.Remove(item, 1);
+
+            return s;
+        }
+        //
     }
 }
 
