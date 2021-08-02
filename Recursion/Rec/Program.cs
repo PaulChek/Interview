38ef245b550e0a9b29ae3827b6f14e4a9ef31f3b
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-08-02 20:11:08 +0700

    recursion

---
+++ Recursion/Rec/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Rec {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(FindFactorial(4));
+        }
+        static int FindFactorial(int n) => _ = n == 0 ? 1 : FindFactorial(n - 1) * n;
+    }
+}
