9a7b1a22df52dc9429e65db0737032becd2b913b
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-08-05 01:36:58 +0700

    LevelinOrder

--- BinaryTree/Trees/Program.cs
+++ BinaryTree/Trees/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Trees {
     class Program {
@@ -6,19 +9,44 @@ namespace Trees {
             var tree = new BTree();
 
             tree.Add(8);
+            tree.Add(-1);
+            tree.Add(-5);
+            tree.Add(-2);
             tree.Add(10);
             tree.Add(5);
             tree.Add(2);
+            tree.Add(6);
+            tree.Add(7);
+            tree.Add(3);
             tree.Add(12);
-            tree.Add(13);
-            tree.Add(14);
+            tree.Add(15);
             tree.Add(1);
             tree.Add(0);
 
+            var res = new List<int[]>();
 
-            var res = tree.Depth();
 
-            Console.WriteLine(res);
+            tree.LevelOrder(res);
+
+            foreach (var item in res)
+                Console.WriteLine($"[ {string.Join(", ", item)} ]");
+
+
+            var res2 = tree.LevelOrder2();
+
+
+            Console.WriteLine("--------Good My InDepth---------");
+
+
+            foreach (var item in res2)
+                Console.WriteLine($"[ {string.Join(", ", item)} ]");
+
+            Console.WriteLine("*********Good Bread First not my*********");
+
+            var res3 = tree.LevelOrder3();
+
+            foreach (var item in res3)
+                Console.WriteLine($"[ {string.Join(", ", item)} ]");
         }
     }
     class BTree {
@@ -59,6 +87,76 @@ namespace Trees {
             var r = Depth(node.Right, depth);
 
             return Math.Max(l, r);   //Math.Max(Depth(node.Left, depth), Depth(node.Right, depth));
+        }
+        public void LevelOrder(List<int[]> ret, Queue<Node> q = null) {
+
+            var res = new List<Node>();
+
+            if (q == null) {
+                q = new Queue<Node>();
+                q.Enqueue(Root);
+            }
+
+            while (q.Count > 0) {
+                var curr = q.Dequeue();
+                if (curr != null) res.Add(curr);
+            }
+
+
+            foreach (var node in res) {
+                if (node.Left != null) q.Enqueue(node.Left);
+                if (node.Right != null) q.Enqueue(node.Right);
+            }
+
+            if (res.Count > 0) ret.Add(res.Select(n => n.Data).ToArray()); // for get result 
+
+            Console.WriteLine(string.Join(" ", res.Select(n => n.Data))); // justt for demonstration
+
+            if (res.Count > 0) LevelOrder(ret, q);
+
+        }
+        public List<List<int>> LevelOrder2() => LevelOrder2(Root, new());
+        private List<List<int>> LevelOrder2(Node node, List<List<int>> res, int depth = 0) {
+            if (node == null) return res;
+
+            if (res.ElementAtOrDefault(depth) != null) res[depth].Add(node.Data);
+            else res.Add(new List<int>() { node.Data });
+
+
+            depth++;
+
+            LevelOrder2(node.Left, res, depth);
+            LevelOrder2(node.Right, res, depth);
+
+            return res;
+        }
+
+        public List<List<int>> LevelOrder3() {
+
+            var q = new Queue<Node>();
+            var levelArr = new List<int>();
+            var res = new List<List<int>>();
+
+            q.Enqueue(Root);
+
+            int l = q.Count, processed = 0;
+
+
+            while (q.Count > 0) {
+
+                var curr = q.Dequeue();
+
+                processed++;
+
+                levelArr.Add(curr.Data);
+
+                if (curr.Left != null) q.Enqueue(curr.Left);
+                if (curr.Right != null) q.Enqueue(curr.Right);
+
+                if (processed == l) { l = q.Count; processed = 0; res.Add(levelArr); levelArr = new List<int>(); }
+
+            }
+            return res;
         }
     }
 }
