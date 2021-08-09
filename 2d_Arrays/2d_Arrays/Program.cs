9c2a15fdfd2b7c0c2d16248c2c85ece571c8c56e
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-08-09 23:45:21 +0700

    shortest path in matrix

---
+++ 2d_Arrays/2d_Arrays/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2d_Arrays {
+    class Program {
+        static void Main(string[] args) {
+
+            // 3 - wall, 2 - start, 1 - finish, return steps
+            var arr2d = new int[,] {
+                { 0, 0, 0, 0, 0 },
+                { 2, 3, 0, 0, 3 },
+                { 0, 2, 3, 1, 3 },
+                { 0, 0, 0, 0, 0 }
+            };
+            List<int> flat = FlatArray(arr2d);
+
+            List<int> flat2 = FlatArray2(arr2d);
+
+
+
+            //solving task about steps
+            var steps = SolveTaskWithShortestPathIn2dArray.AmountOfSteps(arr2d);
+
+            Console.WriteLine(string.Join(" ", steps));
+        }
+
+        private static List<int> FlatArray2(int[,] arr2d) {
+            var q = new Queue<(int r, int c)>(); var dir = new (int r, int c)[] { (-1, 0), (0, 1), (1, 0), (0, -1) };
+            var seen = new HashSet<(int r, int c)>();
+            var res = new List<int>();
+            q.Enqueue((2, 2));
+            seen.Add((2, 2));
+
+            while (q.Count > 0) {
+                var curr = q.Dequeue();
+                res.Add(arr2d[curr.r, curr.c]);
+
+                foreach (var d in dir) {
+                    var new_dir = (curr.r + d.r, curr.c + d.c);
+                    if (new_dir.Item1 >= 0 && new_dir.Item1 < arr2d.GetLength(0) &&
+                        new_dir.Item2 >= 0 && new_dir.Item2 < arr2d.GetLength(1) &&
+                        !seen.Contains(new_dir)) {
+                        seen.Add(new_dir);
+                        q.Enqueue(new_dir);
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        public static List<int> FlatArray(int[,] arr2d) => FlatArray(arr2d, new(), (0, 0), new());
+        private static List<int> FlatArray(int[,] arr2d, HashSet<(int, int)> seen, (int r, int c) current, List<int> res) {
+            if (seen.Contains(current) || current.r < 0 || current.r >= arr2d.GetLength(0) || current.c < 0 || current.c >= arr2d.GetLength(1))
+                return res;
+
+            seen.Add(current);
+            res.Add(arr2d[current.r, current.c]);
+
+            FlatArray(arr2d, seen, (current.r - 1, current.c), res);
+            FlatArray(arr2d, seen, (current.r, current.c + 1), res);
+            FlatArray(arr2d, seen, (current.r + 1, current.c), res);
+            FlatArray(arr2d, seen, (current.r, current.c - 1), res);
+
+            return res;
+        }
+    }
+}
---
+++ 2d_Arrays/2d_Arrays/SolveTaskWithShortestPathIn2dArray.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace _2d_Arrays {
+    static class SolveTaskWithShortestPathIn2dArray {
+        public static List<int> AmountOfSteps(int[,] arr2d) {
+
+            List<(int r, int c)> starts = FindStartsPoints(arr2d);
+
+            List<int> res = new();
+
+            starts.ForEach(s => res.Add(FindPathLength(arr2d, s)));
+
+
+
+            return res;
+        }
+
+        private static int FindPathLength(int[,] arr2d, (int r, int c) p) {
+            bool[,] seen = new bool[arr2d.GetLength(0), arr2d.GetLength(1)];
+
+            (int r, int c, int s) pointer = (p.r, p.c, 0);
+
+            var queue = new Queue<(int r, int c, int d)>();
+
+            queue.Enqueue(pointer);
+
+
+            while (queue.Count > 0) {
+                var cur = queue.Dequeue();
+                if (arr2d[cur.r, cur.c] == 1) return cur.d;
+
+                /*up*/
+                if (isValid((cur.r - 1, cur.c, cur.d), seen, arr2d)) {
+                    seen[cur.r, cur.c] = true; queue.Enqueue((cur.r - 1, cur.c, cur.d + 1));
+                }
+
+                /*right*/
+                if (isValid((cur.r, cur.c + 1, cur.d), seen, arr2d)) {
+                    seen[cur.r, cur.c] = true; queue.Enqueue((cur.r, cur.c + 1, cur.d + 1));
+                }
+                /*down*/
+                if (isValid((cur.r + 1, cur.c, cur.d), seen, arr2d)) {
+                    seen[cur.r, cur.c] = true; queue.Enqueue((cur.r + 1, cur.c, cur.d + 1));
+                }
+                /*left*/
+                if (isValid((cur.r, cur.c - 1, cur.d), seen, arr2d)) {
+                    seen[cur.r, cur.c] = true; queue.Enqueue((cur.r, cur.c - 1, cur.d + 1));
+                }
+            }
+
+
+            return -1;
+        }
+
+        private static bool isValid((int r, int c, int d) cur, bool[,] seen, int[,] arr2d) {
+            if (cur.c < 0 || cur.r < 0 || cur.c >= arr2d.GetLength(1)
+                || cur.r >= arr2d.GetLength(0) || arr2d[cur.r, cur.c] == 3 || seen[cur.r, cur.c])
+                return false;
+
+            return true;
+        }
+
+        private static List<(int r, int c)> FindStartsPoints(int[,] arr2d) {
+            List<(int r, int c)> res = new();
+            for (int r = 0; r < arr2d.GetLength(0); r++)
+                for (int c = 0; c < arr2d.GetLength(1); c++)
+                    if (arr2d[r, c] == 2) res.Add((r, c));
+
+            return res;
+        }
+    }
+}
