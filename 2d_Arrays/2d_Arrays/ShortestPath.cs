82f5454584a6cd4abb625b98b0b573f67286b022
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-08-11 01:30:53 +0700

    new Shortest path

--- 2d_Arrays/2d_Arrays/ShortestPath.cs
+++ 2d_Arrays/2d_Arrays/ShortestPath.cs
@@ -7,8 +7,8 @@ namespace _2d_Arrays {
             List<(int, int)> gates = FindAllGates(arr);
 
 
-            gates.ForEach(g => Solve(arr, g, new bool[arr.GetLength(0), arr.GetLength(1)]) );
-            
+            gates.ForEach(g => Solve(arr, g, new bool[arr.GetLength(0), arr.GetLength(1)]));
+
             return arr;
         }
 
@@ -22,7 +22,11 @@ namespace _2d_Arrays {
         }
 
         private static void Solve(int[,] arr, (int i, int j) p, bool[,] seen, int steps = 0) {
-            arr[p.i, p.j] = Math.Min(arr[p.i, p.j], steps);
+
+            /*dont need cuz add check if steps smaller than data in cell in isValid method
+            arr[p.i, p.j] = Math.Min(arr[p.i, p.j], steps); */
+
+            arr[p.i, p.j] = steps; // new 
             seen[p.i, p.j] = true;
 
             if (isValid((p.i - 1, p.j), arr, seen, steps)) { Solve(arr, (p.i - 1, p.j), seen, steps + 1); }
@@ -32,7 +36,7 @@ namespace _2d_Arrays {
         }
 
         private static bool isValid((int i, int j) p, int[,] arr, bool[,] seen, int steps) {
-            if (p.i < 0 || p.i >= arr.GetLength(0) || p.j < 0 || p.j >= arr.GetLength(1) || arr[p.i, p.j] <= steps|| arr[p.i, p.j] == -1 || seen[p.i, p.j] || arr[p.i, p.j] == 0) return false;
+            if (p.i < 0 || p.i >= arr.GetLength(0) || p.j < 0 || p.j >= arr.GetLength(1) || arr[p.i, p.j] <= steps || arr[p.i, p.j] == -1 || seen[p.i, p.j] || arr[p.i, p.j] == 0) return false;
             return true;
         }
     }
