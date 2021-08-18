eed75fa0359c1d390d1e88ef59cda4439949606e
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-08-19 03:23:44 +0700

    NQueensBackTrackingSolution

--- BackTracking/BT/Program.cs
+++ BackTracking/BT/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BT {
     class Program {
@@ -11,77 +12,55 @@ namespace BT {
             sudoku[1 + 3, 1 + 3] = 5;
             sudoku[1 + 3, 2 + 3] = 6;
             sudoku[2 + 3, 0 + 3] = 7;
-            // sudoku[2 + 3, 1 + 3] = 8;
             sudoku[2 + 3, 2 + 3] = 9;
+            //SudokuSolver.Solve(sudoku);
 
 
 
-            SudokuSolver.Solve(sudoku);
+            var queens = 6;
+            var board = new int[queens, queens];
 
-            ShowSudoku(sudoku);
+            N_QueenTask.Solve(board);
+            ShowBoard(board);
 
         }
-        public static void ShowSudoku(int[,] sudocku) {
-            for (int r = 0; r < sudocku.GetLength(0); r++) {
-                for (int c = 0; c < sudocku.GetLength(1); c++)
-                    Console.Write(sudocku[r, c] + " " + (c == 2 || c == 5 || c == 8 ? " " : ""));
-                Console.WriteLine((r == 2 || r == 5 || r == 8 ? "\n" : ""));
+
+        public static void ShowBoard(int[,] board) {
+            for (int i = 0; i < board.GetLength(0); i++) {
+                for (int j = 0; j < board.GetLength(1); j++) {
+                    Console.Write(board[i, j] + "|");
+                }
+                Console.WriteLine();
             }
         }
     }
-    static class SudokuSolver {
-        public static bool Solve(int[,] sudoku) {
-            
-            
-            
-            (int r, int c) = FindEmptyCell( sudoku);
-           
-            
-            if (r == -1) return true;
+    class N_QueenTask {
+        public static bool Solve(int[,] board, int c = 0) {
 
-         
+            if (c >= board.GetLength(1)) return true;
 
 
-            for (int n = 1; n < 10; n++) {
-                if (IsValid((r, c), sudoku, n)) {
-                    sudoku[r, c] = n;
-                   
-                    if (Solve(sudoku)) return true;
-                    else sudoku[r, c] = 0;
+
+            for (int r = 0; r < board.GetLength(1); r++) {
+                if (IsValidPosition(board, (r, c))) {
+                    board[r, c] = 1;
+                    if (Solve(board, c + 1)) return true;
+                    else board[r, c] = 0;
                 }
             }
 
             return false;
-
         }
 
-        private static (int r, int c) FindEmptyCell(int[,] sudoku) {
+        private static bool IsValidPosition(int[,] board, (int r, int c) p) {
+            for (int c = 0; c < p.c; c++)
+                if (board[p.Item1, c] == 1) return false;
 
-            for (int i = 0; i < sudoku.GetLength(0); i++)
-                for (int j = 0; j < sudoku.GetLength(1); j++)
-                    if (sudoku[i, j] == 0) return (i, j);
+            for (int i = p.r, j = p.c; i >= 0 && j >= 0; i--, j--)
+                if (board[i, j] == 1) return false;
 
-            return (-1, -1);
-        }
-
-        static bool IsValid((int row, int col) place, int[,] sudoku, int val) {
-
-            //row 
-            for (int c = 0; c < sudoku.GetLength(0); c++)
-                if (sudoku[place.row, c] == val) return false;
-            //col
-            for (int r = 0; r < sudoku.GetLength(0); r++)
-                if (sudoku[r, place.col] == val) return false;
-
-
-
-            //find Start of cube 3x3;
-            (int sR, int sC) = (place.row / 3 * 3, place.col / 3 * 3); // Math.flor cuz int
-
-            for (int i = sR; i < sR + 3; i++)
-                for (int j = sC; j < sC + 3; j++)
-                    if (sudoku[i, j] == val) return false;
-
+            for (int i = p.r, j = p.c; i < board.GetLength(0) && j >= 0; i++, j--)
+                if (board[i, j] == 1) return false;
 
             return true;
         }
---
+++ BackTracking/BT/SudokuSolver.cs
@@ -0,0 +1,59 @@
+﻿namespace BT {
+    static class SudokuSolver {
+        public static bool Solve(int[,] sudoku) {
+            
+            
+            
+            (int r, int c) = FindEmptyCell( sudoku);
+           
+            
+            if (r == -1) return true;
+
+         
+
+
+            for (int n = 1; n < 10; n++) {
+                if (IsValid((r, c), sudoku, n)) {
+                    sudoku[r, c] = n;
+                   
+                    if (Solve(sudoku)) return true;
+                    else sudoku[r, c] = 0;
+                }
+            }
+
+            return false;
+
+        }
+
+        private static (int r, int c) FindEmptyCell(int[,] sudoku) {
+
+            for (int i = 0; i < sudoku.GetLength(0); i++)
+                for (int j = 0; j < sudoku.GetLength(1); j++)
+                    if (sudoku[i, j] == 0) return (i, j);
+
+            return (-1, -1);
+        }
+
+        static bool IsValid((int row, int col) place, int[,] sudoku, int val) {
+
+            //row 
+            for (int c = 0; c < sudoku.GetLength(0); c++)
+                if (sudoku[place.row, c] == val) return false;
+            //col
+            for (int r = 0; r < sudoku.GetLength(0); r++)
+                if (sudoku[r, place.col] == val) return false;
+
+
+
+            //find Start of cube 3x3;
+            (int sR, int sC) = (place.row / 3 * 3, place.col / 3 * 3); // Math.flor cuz int
+
+            for (int i = sR; i < sR + 3; i++)
+                for (int j = sC; j < sC + 3; j++)
+                    if (sudoku[i, j] == val) return false;
+
+
+            return true;
+        }
+    }
+}
